4410348ff89b5d4427073cfce2a5721324b46ca2
Markus Kostrzewski <ciapa@ciapa.tech>
2020-02-02 12:54:29 +0100

    Update websocket endpoint

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -53,7 +53,7 @@ export class GameScene extends Phaser.Scene {
     this._displayOpponentCardBacks();
     this.onResetGrid();
     this._clock = new Phaser.Time.Clock(this);
-      this._ws = new WebSocket('wss://overengineer.wtf/backend/join');
+      this._ws = new WebSocket('ws://overengineer.wtf:8080/backend/join');
     this._ws.onopen = (event) => {
       this.onConnectToGame(event);
       this._lastBeat = this._clock.now;
